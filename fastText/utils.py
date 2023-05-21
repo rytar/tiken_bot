a9a7391e67caec9df08402aab78e12cec8301e08
rytar <ryou79pd@gmail.com>
2023-05-22 00:51:21 +0900

    create shel script

--- fastText/utils.py
+++ fastText/utils.py
@@ -1,6 +1,24 @@
 import emoji
 import regex
 
+def get_text(note: dict):
+    text: str = ''
+
+    if note["cw"] is not None:
+        text += note["cw"] + '\n'
+    
+    if note["text"] is not None:
+        text += note["text"] + '\n'
+
+    if note["renoteId"] is not None:
+        if note["renote"]["cw"] is not None:
+            text += note["renote"]["cw"] + '\n'
+
+        if note["renote"]["text"] is not None:
+            text += note["renote"]["text"] + '\n'
+    
+    return text
+
 def get_reaction_name(reaction: str):
     m = regex.fullmatch(r"^:([^:@]+)@([^@:]+):$", reaction)
     if m:
